3a9756fc133e9d7364c9d740a592b816e028dcbb
Ordonnaud <christophe.ordonnaud@expleogroup.com>
2021-06-15 09:24:55 +0200

    Résolution Issue 1 : fermer la modale

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,23 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeSelect = document.querySelector(".close"); // declaration du bouton (ici une croix) à actionner
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// close modal event ==> ecoute l'evenement sur closSelect et renvoi la fonction closeModal si il y a un click
+closeSelect.addEventListener("click", closeModal);
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
+
+// close modal form ==> active le display du 'div class="bground"' à "none" => la fenêtre disparaît
+function closeModal() {
+  modalbg.style.display = "none";
+}
+
 
 
