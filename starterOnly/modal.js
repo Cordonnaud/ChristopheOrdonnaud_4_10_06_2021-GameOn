15e1c8ddfe640d5272ef69ab1c6d277374c2e7c2
Ordonnaud <christophe.ordonnaud@expleogroup.com>
2021-06-21 16:50:31 +0200

    #4-4

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -59,6 +59,14 @@ const form = document .getElementById("formulaire");
 
 let mailCaractere = /[a-z0-9_\-\.]+@[a-z0-9_\-\.]+\.[a-z]+/i;
 
+let today = new Date()
+const ddate = today.getDate();
+const mdate = today.getMonth() + 1;
+const ydate = today.getFullYear();
+console.log(ddate, mdate,ydate);
+console.log(today);
+
+
 
 //--#2 ---------------------------------------------------------------------------------------
 form.addEventListener("submit", val);
@@ -67,7 +75,7 @@ function val(){
   {
     alert ("Documentez votre prénom avec 2 lettres minimums");
     //--#3------ Ajouter validation ou messages d'erreur #3  ---------
-    firstErr.textContent =" Veuillez renseigner votre prénom (2 lettres mini !)";
+    firstErr.textContent ="Veuillez renseigner votre prénom (2 lettres mini !)";
     firstErr.style.fontSize = "12px"; 
     firstErr.style.background ="yellow"; 
     firstErr.style.color ="black"; 
@@ -82,7 +90,7 @@ function val(){
   {
     alert ("Documentez votre nom avec 2 lettres minimums"); 
     //--#3------ Ajouter validation ou messages d'erreur #3  ---------
-    lastErr.textContent =" Veuillez renseigner votre nom (2 lettres mini !)";
+    lastErr.textContent ="Veuillez renseigner votre nom (2 lettres mini !)";
     lastErr.style.fontSize = "12px"; 
     lastErr.style.background ="yellow"; 
     lastErr.style.color ="black";  
@@ -93,7 +101,6 @@ function val(){
     // Pas de message d'erreur, nombre de caractères suffisant
   }
   
-
   if(mailCaractere.test(email.value)){
     emailErr.textContent ="";
     // Pas de message d'erreur, caracteres valides
@@ -101,7 +108,7 @@ function val(){
   else{
     alert ("Documentez une adresse mail valide "); 
     //--#3------ Ajouter validation ou messages d'erreur #3  ---------
-    emailErr.textContent =" Veuillez renseigner votre adresse mail!)";
+    emailErr.textContent ="Veuillez renseigner votre adresse mail!";
     emailErr.style.fontSize = "12px"; 
     emailErr.style.background ="yellow"; 
     emailErr.style.color ="black";   
@@ -111,22 +118,24 @@ function val(){
   if (birth.value ===''){
     alert("Documentez votre date de naissance");
     //--#3------ Ajouter validation ou messages d'erreur #3  ---------
-    birthErr.textContent =" Veuillez renseigner votre date de naissance!)";
+    birthErr.textContent ="Veuillez renseigner votre date de naissance!";
     birthErr.style.fontSize = "12px"; 
     birthErr.style.background ="yellow"; 
     birthErr.style.color ="black"; 
     event.preventDefault();
+ 
   }
-
   else{
-    birthErr.textContent ="";
+    birthErr.textContent =" ";
+
+    
     //  Pas de message d'erreur, une date est renseignée
   }
 
   if (tournamentNb.value === ''){
     alert ("Documentez un nombre de tournois GameOn");
     //--#3------ Ajouter validation ou messages d'erreur #3  ---------
-    tournamentNbErr.textContent =" Veuillez renseigner le nombre de vos participation à un tournois GameOne!)";
+    tournamentNbErr.textContent ="Veuillez renseigner le nombre de vos participation à un tournois GameOne!";
     tournamentNbErr.style.fontSize = "12px"; 
     tournamentNbErr.style.background ="yellow"; 
     tournamentNbErr.style.color ="black"; 
@@ -144,7 +153,7 @@ function val(){
   else{
     alert ("Selectionnez une ville"); 
     //--#3------ Ajouter validation ou messages d'erreur #3  ---------
-    tournamentLocErr.textContent =" Veuillez choisir une ville!)";
+    tournamentLocErr.textContent =" Veuillez choisir une ville!";
     tournamentLocErr.style.fontSize = "12px"; 
     tournamentLocErr.style.background ="yellow"; 
     tournamentLocErr.style.color ="black"; 
@@ -154,19 +163,53 @@ function val(){
   if(checkbox1.checked){
     conditionErr.textContent ="";
   //   Pas de message d'erreur, la case Condition selectionnée
- 
   }
   else{
     alert("Veuillez accepter les condtions d'utilisation");
     //--#3------ Ajouter validation ou messages d'erreur #3  ---------
-    conditionErr.textContent =" Veuillez vérifier que vous acceptez les termes et conditions.!)";
+    conditionErr.textContent =" Veuillez vérifier que vous acceptez les termes et conditions!";
     conditionErr.style.fontSize = "12px"; 
     conditionErr.style.background ="red"; 
     conditionErr.style.color ="black"; 
     event.preventDefault();
   }
 }
-//--#3--------- Ajouter validation ou messages d'erreur #3  ---------
+ 
+// birth.addEventListener("input", valdate);
+// console.log(input.birth.value);
+// let birthday = new Date(birth.addEventListener("input", valdate));
+// let dbirth = birthday.getDate();
+// let mdbirth = birthday.getMonth() + 1;
+// let ybirth = birthday.getFullYear();
+// console.log(dbirth, mdbirth, ybirth);
+
+function valdate(){
+  birth.addEventListener("input", valdate);
+  console.log(input.birth.value);
+  let birthday = new Date(birth.addEventListener("input", valdate));
+  let dbirth = birthday.getDate();
+  let mdbirth = birthday.getMonth() + 1;
+  let ybirth = birthday.getFullYear();
+  console.log(dbirth, mdbirth, ybirth);
+  if( ydate<ybirth && mdate<mbirth && ddate<dbirth){
+    alert("Veuillez modifier la date");
+    birthErr.textContent =" Problème de date !";
+    birthErr.style.fontSize = "12px"; 
+    birthErr.style.background ="blue"; 
+    birthErr.style.color ="white";
+    console.log(dbirth, mdbirth, ybirth); 
+    event.preventDefault();
+}
+else{
+  birthErr.textContent =" ";
+  console.log(dbirth, mdbirth, ybirth);
+  //  Pas de message d'erreur, une date est renseignée
+  }
+}
+   
+
+
+
 
 //--#4--------- Ajouter confirmation quand envoie réussi #4  ---------
 confirme.addEventListener("click", function conf() {
@@ -174,7 +217,7 @@ confirme.addEventListener("click", function conf() {
     last.value && 
     birth.value && 
     email.value && 
-    tournamentNb && 
+    tournamentNb.value && 
     ((tournamentLoc0.checked) || 
     (tournamentLoc1.checked) || 
     (tournamentLoc2.checked) || 
@@ -188,7 +231,7 @@ confirme.addEventListener("click", function conf() {
   }
   else{
     alert("C'est la loose");
-  }
-});
+  }})
+
 
 
